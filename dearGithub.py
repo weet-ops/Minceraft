733c06afb71de1962569220ecc725faded439c74
AmanitaFungifin <115834022+weet-ops@users.noreply.github.com>
2026-02-23 15:56:25 -0600

    Python file
    This is here so GitHub can say that this was programmed in Python. And it was.

---
+++ dearGithub.py
@@ -0,0 +1,6 @@
+# Dear GitHub, I am uploading this file (that someone might currently be reading) as a way to prove that this repository was programmed in Python and nothing but Python. Thank you for your understanding
+import time
+
+while True:
+    time.sleep(0)
+    print("GITHUB RULES!!!")
